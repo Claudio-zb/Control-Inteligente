8b7d515d6d67c731e4765f8062b4c83b3347bdfb
Christian Diaz <christian.diaz.g@ug.uchile.cl>
2023-04-09 21:12:02 -0400

    add modelo linealizado

---
+++ Ej1.1/paramsLinealizado.m
@@ -0,0 +1,13 @@
+%Parametros de siempre
+P_cp = 1924; % [kJ/l°C]
+H_l = 1.05; % [kW/°C]
+C = 2267; % [%kW/°C]
+n0_S = 1322; % [m2]
+Tinit = 260; % [°C] Temperatura inicial de salida del aceite
+
+%Condiciones iniciales
+Irr_init = 920; % [W/m2]
+Tout_init = 260; % [°C]
+Tin_init = 185; % [°C]
+Tamb_init = 25; % [°C]
+V_init = 7; % [l/s]
